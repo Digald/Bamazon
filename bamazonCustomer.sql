b176fdb24003c451ad273fc8403fb655d047f964
Mark Alaniz <markalaniz000@gmail.com>
2018-01-25 22:04:30 -0600

    created database and seed for products

---
+++ bamazonCustomer.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS bamazon;
+CREATE DATABASE bamazon;
+USE bamazon;
+
+CREATE TABLE products(
+    id INT AUTO_INCREMENT NOT NULL,
+    product_name VARCHAR(100),
+    department_name VARCHAR(100),
+    price FLOAT(10,2),
+    stock_quantity INT(10),
+    PRIMARY KEY (id)
+);
+
---
+++ bamazonCustomerSeed.sql
@@ -0,0 +1,11 @@
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES
+('Refined Steel Ball', 'Toys', 778.54, 20),
+('Practical Plastic Towels', 'Clothes', 147.45, 15),
+('Fantastic Wooden Keyboard', 'Electronics', 817.31, 45),
+('Tasty Fresh Chips', 'Arts and Crafts', 743.82, 10),
+('Licensed Metal Chicken', 'Arts and Crafts', 372.15, 13),
+('Handmade Rubber Bike', 'Toys', 327.00, 34),
+('Generic Frozen Towels', 'Clothes', 136.74, 3),
+('Wooden Computer', 'Electronics', 270.00, 4),
+('Incredible Metal Chicken', 'Toys', 86.45, 7),
+('Handcrafted Granite Towels', 'Electronics', 904.00, 2);
