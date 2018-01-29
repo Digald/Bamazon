8a3fa9646400ecc4e80a139ca515a9cb8a36b80f
Mark Alaniz <markalaniz000@gmail.com>
2018-01-28 21:47:56 -0600

    working product sales by department table, now going to make inquirer prompt

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -13,28 +13,35 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
-  productSales();
+  productSales()
 });
 
 function productSales() {
   connection.query(
     "SELECT departments.department_id, departments.department_name, departments.over_head_costs, products.product_sales FROM products left JOIN departments ON products.department_name = departments.department_name GROUP BY department_name",
     function(err, res) {
-        if (err){
-            return console.log(err);
-        }
-        var table = [];
-        for (var i = 0; i < res.length; i++){
-            table.push(Object.values(res[i]));
-        }
-        var tableHeading = Object.keys(res[0]);
-        tableHeading.push('total_profit');
-        
-        var wt = new WordTable(tableHeading, table);
-        console.log(wt.string());
+      if (err) {
+        return console.log(err);
+      }
+      var table = [];
+
+      // add the total_profit header to the table
+      var tableHeading = Object.keys(res[0]);
+      tableHeading.push("total_profit");
+
+      for (var i = 0; i < res.length; i++) {
+        var data = Object.values(res[i]);
+        var total_profit = Object.values(res[i])[3] - Object.values(res[i])[2];
+        data.push(total_profit.toFixed(2));
+        table.push(data);
+      }
+      // display table
+      var wt = new WordTable(tableHeading, table);
+      console.log(wt.string());
     }
   );
 }
+
 function displaySales() {
   connection.query("SELECT * FROM departments", function(err, res) {
     console.log(res);
